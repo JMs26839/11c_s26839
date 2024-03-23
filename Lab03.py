84c3e9de38c478529525ba5f3e47144f1ce9ae01
jakubmordal <s26839@pjwstk.edu.pl>
2024-03-23 18:12:38 +0100

    Task 2

--- Lab03.py
+++ Lab03.py
@@ -3,3 +3,9 @@ import math
 squares_list=[x**2 for x in range(1,10)]
 print(squares_list)
 
+#task_2
+def square_list_from_range(start, end):
+    list=[x**2 for x in range(start,end)]
+    return list
+#task2 test
+print(square_list_from_range(1,10))
